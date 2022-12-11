08eb16bb1a29f01d40d5c49575a0f824fceaf41d
Oana Madalina Fron <o.m.fron-1@student.tudelft.nl>
2022-12-11 15:50:04 +0100

    Create notification microservice

---
+++ notification-microservice/src/main/java/nl/tudelft/sem/template/notification/Application.java
@@ -0,0 +1,14 @@
+package nl.tudelft.sem.template.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Notification microservice application.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
