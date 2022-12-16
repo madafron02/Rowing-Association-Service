1f3127a28c51a0fd8bb589c7b98210dc051b4eb5
Adomas Bagdonas <A.Bagdonas@student.tudelft.nl>
2022-12-16 18:52:42 +0100

    Refactor the Activity entity
    - Use annotations to avoid boilerplate code.
    - Remove constructor that uses a data model.
    - Use the newly created embeddable classes.

--- activities-microservice/src/main/java/nl/tudelft/sem/template/activities/domain/Activity.java
+++ activities-microservice/src/main/java/nl/tudelft/sem/template/activities/domain/Activity.java
@@ -4,21 +4,26 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Embedded;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import nl.tudelft.sem.template.activities.model.ActivityDataModel;
+import lombok.Setter;
 
 /**
  * A DDD entity representing an activity in our domain.
  */
 @Entity
-@Table(name = "users")
+@Table(name = "activities")
 @NoArgsConstructor
 @EqualsAndHashCode
+@Getter
+@Setter
 public class Activity {
 
     @Id
@@ -29,26 +34,11 @@ public class Activity {
     @Column(name = "ownerId", nullable = false)
     private String ownerId;
 
-    @Column(name = "coxCount")
-    private Integer coxCount;
+    @Embedded
+    private Positions positions;
 
-    @Column(name = "coachCount")
-    private Integer coachCount;
-
-    @Column(name = "portSideRowerCount")
-    private Integer portSideRowerCount;
-
-    @Column(name = "starboardSideRowerCount")
-    private Integer starboardSideRowerCount;
-
-    @Column(name = "scullingRowerCount")
-    private Integer scullingRowerCount;
-
-    @Column(name = "startTime", nullable = false)
-    private LocalDateTime startTime;
-
-    @Column(name = "endTime", nullable = false)
-    private LocalDateTime endTime;
+    @Embedded
+    private Timeslot timeslot;
 
     @Column(name = "certificate")
     private String certificate;
@@ -78,35 +68,12 @@ public class Activity {
                     Integer starboardSideRowerCount, Integer scullingRowerCount, LocalDateTime startTime,
                     LocalDateTime endTime, String certificate, Boolean competition, String gender) {
         this.ownerId = ownerId;
-        this.coxCount = coxCount;
-        this.coachCount = coachCount;
-        this.portSideRowerCount = portSideRowerCount;
-        this.starboardSideRowerCount = starboardSideRowerCount;
-        this.scullingRowerCount = scullingRowerCount;
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.positions = new Positions(coxCount, coachCount, portSideRowerCount, starboardSideRowerCount,
+                scullingRowerCount);
+        this.timeslot = new Timeslot(startTime, endTime);
         this.certificate = certificate;
         this.competition = Objects.requireNonNullElse(competition, false);
         this.gender = gender;
-    }
-
-    /**
-     * Constructs a new activity from a request data model.
-     *
-     * @param dataModel the activity data from the request
-     */
-    public Activity(ActivityDataModel dataModel) {
-        this.ownerId = dataModel.getOwnerId();
-        this.coxCount = dataModel.getCoxCount();
-        this.coachCount = dataModel.getCoachCount();
-        this.portSideRowerCount = dataModel.getPortSideRowerCount();
-        this.starboardSideRowerCount = dataModel.getStarboardSideRowerCount();
-        this.scullingRowerCount = dataModel.getScullingRowerCount();
-        this.startTime = dataModel.getStartTime();
-        this.endTime = dataModel.getEndTime();
-        this.certificate = dataModel.getCertificate();
-        this.competition = dataModel.getCompetition();
-        this.gender = dataModel.getGender();
     }
 
     /**
@@ -115,109 +82,14 @@ public class Activity {
      * @return true if this is valid and false otherwise
      */
     public boolean checkIfValid() {
-        boolean requiresRowers = coxCount != null || coachCount != null || portSideRowerCount != null
-                || starboardSideRowerCount != null || scullingRowerCount != null;
-        boolean nonNull = ownerId != null && requiresRowers && startTime != null;
+        boolean requiresRowers = positions.getCoxCount() != null || positions.getCoachCount() != null
+                || positions.getPortSideRowerCount() != null || positions.getStarboardSideRowerCount() != null
+                || positions.getScullingRowerCount() != null;
+        boolean nonNull = ownerId != null && requiresRowers && timeslot.getStartTime() != null;
         if (!nonNull) {
             return false;
         }
         LocalDateTime now = LocalDateTime.now();
-        return startTime.isBefore(endTime) && startTime.isAfter(now);
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(String ownerId) {
-        this.ownerId = ownerId;
-    }
-
-    public Integer getCoxCount() {
-        return coxCount;
-    }
-
-    public void setCoxCount(Integer coxCount) {
-        this.coxCount = coxCount;
-    }
-
-    public Integer getCoachCount() {
-        return coachCount;
-    }
-
-    public void setCoachCount(Integer coachCount) {
-        this.coachCount = coachCount;
-    }
-
-    public Integer getPortSideRowerCount() {
-        return portSideRowerCount;
-    }
-
-    public void setPortSideRowerCount(Integer portSideRowerCount) {
-        this.portSideRowerCount = portSideRowerCount;
-    }
-
-    public Integer getStarboardSideRowerCount() {
-        return starboardSideRowerCount;
-    }
-
-    public void setStarboardSideRowerCount(Integer starboardSideRowerCount) {
-        this.starboardSideRowerCount = starboardSideRowerCount;
-    }
-
-    public Integer getScullingRowerCount() {
-        return scullingRowerCount;
-    }
-
-    public void setScullingRowerCount(Integer scullingRowerCount) {
-        this.scullingRowerCount = scullingRowerCount;
-    }
-
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalDateTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalDateTime endTime) {
-        this.endTime = endTime;
-    }
-
-    public String getCertificate() {
-        return certificate;
-    }
-
-    public void setCertificate(String certificate) {
-        this.certificate = certificate;
-    }
-
-    public boolean isCompetition() {
-        return competition;
-    }
-
-    public void setCompetition(boolean competition) {
-        this.competition = competition;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
+        return timeslot.getStartTime().isBefore(timeslot.getEndTime()) && timeslot.getEndTime().isAfter(now);
     }
 }
