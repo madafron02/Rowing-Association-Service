2f051291ce1700b2d8e182837871f4de8941c1c0
Adomas Bagdonas <A.Bagdonas@student.tudelft.nl>
2022-12-16 18:43:47 +0100

    Create classes to embed in an Activity entity

---
+++ activities-microservice/src/main/java/nl/tudelft/sem/template/activities/domain/Positions.java
@@ -0,0 +1,32 @@
+package nl.tudelft.sem.template.activities.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Positions {
+
+    @Column(name = "coxCount")
+    private Integer coxCount;
+
+    @Column(name = "coachCount")
+    private Integer coachCount;
+
+    @Column(name = "portSideRowerCount")
+    private Integer portSideRowerCount;
+
+    @Column(name = "starboardSideRowerCount")
+    private Integer starboardSideRowerCount;
+
+    @Column(name = "scullingRowerCount")
+    private Integer scullingRowerCount;
+}
---
+++ activities-microservice/src/main/java/nl/tudelft/sem/template/activities/domain/Timeslot.java
@@ -0,0 +1,24 @@
+package nl.tudelft.sem.template.activities.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.time.LocalDateTime;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Timeslot {
+
+    @Column(name = "startTime", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "endTime", nullable = false)
+    private LocalDateTime endTime;
+}
