6850ae3f06379dc34bd938c027e33eeff8aac734
Mitchell van Pelt <m.vanpelt-1@student.tudelft.nl>
2022-12-16 16:56:01 +0100

    Added ChainCreator as a helper method for the Controller.

--- auth-microservice/src/main/java/nl/tudelft/sem/template/auth/controllers/AuthenticationController.java
+++ auth-microservice/src/main/java/nl/tudelft/sem/template/auth/controllers/AuthenticationController.java
@@ -1,11 +1,9 @@
 package nl.tudelft.sem.template.auth.controllers;
 
-import nl.tudelft.sem.template.auth.application.handlers.CreateAccount;
-import nl.tudelft.sem.template.auth.application.handlers.CreateToken;
-import nl.tudelft.sem.template.auth.application.handlers.ExceptionHandler;
-import nl.tudelft.sem.template.auth.application.handlers.SanitizeCredentials;
+import nl.tudelft.sem.template.auth.application.handlers.*;
 import nl.tudelft.sem.template.auth.domain.AccountCredentials;
 import nl.tudelft.sem.template.auth.domain.AccountsRepo;
+import nl.tudelft.sem.template.auth.domain.ChainCreator;
 import nl.tudelft.sem.template.auth.models.RegistrationRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -46,22 +44,10 @@ public class AuthenticationController {
      */
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody RegistrationRequestModel request) throws Exception {
-
         AccountCredentials credentials = new AccountCredentials(request.getUserId(), request.getPassword());
 
         ExceptionHandler exceptionHandler = new ExceptionHandler();
-        SanitizeCredentials sanitizeCredentials = new SanitizeCredentials();
-        CreateAccount createAccount = new CreateAccount(accountsRepo);
-        CreateToken createToken = new CreateToken(jwtSecret);
-
-        sanitizeCredentials.setExceptionHandler(exceptionHandler);
-        createAccount.setExceptionHandler(exceptionHandler);
-        createToken.setExceptionHandler(exceptionHandler);
-
-        sanitizeCredentials.setNext(createAccount);
-        createAccount.setNext(createToken);
-
-        sanitizeCredentials.handle(credentials);
+        CreateToken createToken = ChainCreator.createRegistrationChain(exceptionHandler, accountsRepo, jwtSecret, credentials);
 
         String token = createToken.getToken();
         if(exceptionHandler.didCatchException() || token == null){
