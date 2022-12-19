c2239e1192c170217008bae693dbdbf51c2578a8
Adomas Bagdonas <A.Bagdonas@student.tudelft.nl>
2022-12-19 11:58:32 +0100

    Add missing Javadoc comments

--- activities-microservice/src/main/java/nl/tudelft/sem/template/activities/domain/ActivityRepository.java
+++ activities-microservice/src/main/java/nl/tudelft/sem/template/activities/domain/ActivityRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+/**
+ * A DDD JPA repository for the Activity entity.
+ */
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
