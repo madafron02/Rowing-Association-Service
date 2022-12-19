0a8bb8b40eb3c5ebfbcf1930e0ba8dab17628058
Micloiu Diana <D.Micloiu@student.tudelft.nl>
2022-12-19 23:51:20 +0100

    Test certificate and competitiveness handlers

---
+++ matching-microservice/src/test/java/nl/tudelft/sem/template/matching/domain/handlers/CompetitivenessHandlerTest.java
@@ -0,0 +1,68 @@
+package nl.tudelft.sem.template.matching.domain.handlers;
+
+import nl.tudelft.sem.template.matching.domain.ActivityApp;
+import nl.tudelft.sem.template.matching.domain.MatchFilter;
+import nl.tudelft.sem.template.matching.domain.TimeslotApp;
+import nl.tudelft.sem.template.matching.domain.TypeOfActivity;
+import nl.tudelft.sem.template.matching.domain.UserApp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class CompetitivenessHandlerTest {
+
+    private FilteringHandler filteringHandler;
+    private MatchFilter matchFilter;
+    private ActivityApp activityApp;
+    private UserApp user;
+
+    @BeforeEach
+    void setUp() {
+        filteringHandler = new CompetitivenessHandler();
+        HashMap<String, Integer> positions = new HashMap<>();
+        positions.put("cox", 2);
+
+        activityApp = new ActivityApp(3L,
+                "l.tosa@tudelft.nl",
+                new TimeslotApp(LocalDateTime.parse("2022-12-08T13:15"),
+                        LocalDateTime.parse("2022-12-08T14:00")),
+                "Male", "SEM", positions, false,
+                TypeOfActivity.COMPETITION, "4+");
+        user = new UserApp("d.micloiu@icloud.com", "C4",
+                "Female", "SEM", true);
+
+        matchFilter = new MatchFilter(activityApp, user, "coach",
+                new TimeslotApp(LocalDateTime.parse("2022-12-08T10:15"),
+                        LocalDateTime.parse("2022-12-08T17:00")));
+    }
+
+    @Test
+    void handleTrue() {
+        // competitive user for uncompetitive activity
+        assertThat(filteringHandler.handle(matchFilter)).isTrue();
+
+        // competitive user for competitive activity
+        activityApp.setCompetitiveness(true);
+        assertThat(filteringHandler.handle(matchFilter)).isTrue();
+
+        // uncompetitive user for uncompetitive activity
+        activityApp.setCompetitiveness(false);
+        user.setCompetitiveness(false);
+        assertThat(filteringHandler.handle(matchFilter)).isTrue();
+
+        filteringHandler.setNext(new GenderHandler());
+        assertThat(filteringHandler.handle(matchFilter)).isFalse();
+    }
+
+    @Test
+    void handleFalse() {
+        // uncompetitive user for competitive activity
+        user.setCompetitiveness(false);
+        activityApp.setCompetitiveness(true);
+        assertThat(filteringHandler.handle(matchFilter)).isFalse();
+    }
+}
