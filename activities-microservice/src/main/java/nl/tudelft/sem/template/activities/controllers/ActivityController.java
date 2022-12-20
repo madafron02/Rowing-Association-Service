5152e18afb33121d02db8613f5e6f5de0a744f8c
Adomas Bagdonas <A.Bagdonas@student.tudelft.nl>
2022-12-20 19:22:55 +0100

    Add activity editing endpoint

--- activities-microservice/src/main/java/nl/tudelft/sem/template/activities/controllers/ActivityController.java
+++ activities-microservice/src/main/java/nl/tudelft/sem/template/activities/controllers/ActivityController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -112,5 +113,27 @@ public class ActivityController {
         activityRepository.delete(toDelete.get());
         matchingClient.deleteAllMatches(activityId);
         return ResponseEntity.ok("Activity with the id " + activityId + " has been deleted successfully!");
+    }
+
+    /**
+     * Updates the attributes of an Activity.
+     *
+     * @param request the values which will be used for updating the Activity
+     * @return the Activity that was updated
+     */
+    @PatchMapping("/edit")
+    public ResponseEntity<Activity> updateActivity(@RequestBody Activity request) {
+        Optional<Activity> toUpdate = activityRepository.findById(request.getId());
+        if (toUpdate.isEmpty()) {
+            return new ResponseEntity("Activity with the id: " + request.getId() + " was not found.", HttpStatus.NOT_FOUND);
+        }
+        Activity activity = toUpdate.get();
+        activity.updateFields(request);
+        if (!activity.checkIfValid()) {
+            return new ResponseEntity("Update failed: the attributes have incorrect values.", HttpStatus.BAD_REQUEST);
+        }
+        activityRepository.save(activity);
+        matchingClient.deleteAllMatches(activity.getId());
+        return ResponseEntity.ok(activity);
     }
 }
