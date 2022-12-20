f8a23846e2cff43a4ff56809880d9b3a0ed611f5
Oana Madalina Fron <o.m.fron-1@student.tudelft.nl>
2022-12-20 23:48:08 +0200

    Add @Data annotation

--- notification-microservice/src/main/java/nl/tudelft/sem/template/notification/domain/Timeslot.java
+++ notification-microservice/src/main/java/nl/tudelft/sem/template/notification/domain/Timeslot.java
@@ -1,74 +1,18 @@
 package nl.tudelft.sem.template.notification.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 /**
  * Timeslot object meant to specify start time and end time of an activity.
  */
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Timeslot {
-    private final LocalDateTime startTime;
-    private final LocalDateTime endTime;
-
-    /**
-     * Gets start time.
-     *
-     * @return the start time
-     */
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    /**
-     * Gets end time.
-     *
-     * @return the end time
-     */
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-
-    /**
-     * Check equality of timeslots.
-     *
-     * @param o the object to be checked if equal to this one
-     * @return true if equal, false otherwise
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Timeslot)) {
-            return false;
-        }
-        Timeslot timeslot = (Timeslot) o;
-        return Objects.equals(startTime, timeslot.startTime) && Objects.equals(endTime, timeslot.endTime);
-    }
-
-    /**
-     * Gets hash code.
-     *
-     * @return hash code
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(startTime, endTime);
-    }
-
-    /**
-     * Gets timeslot as sting.
-     *
-     * @return timeslot as string
-     */
-    @Override
-    public String toString() {
-        return "Timeslot{"
-                + "start=" + startTime
-                + ", end=" + endTime
-                + '}';
-    }
+    private transient LocalDateTime startTime;
+    private transient LocalDateTime endTime;
 }
